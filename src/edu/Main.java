75e8c1e69644ce244454d2a1cd1d5efb7b514ed8
babydumbo <51706450+babydumbo@users.noreply.github.com>
2021-12-09 16:01:19 +0900

    init project

---
+++ src/edu/Main.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package edu;
+
+/**
+ * @author akwld
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//변수 선언하기
+		int firstNo, secondNo;
+		int sum;
+		
+		//두수 입력하기
+		firstNo=10;
+		secondNo=20;
+		
+		// 합계 내기
+		sum=firstNo+secondNo;
+		
+		// 합계 출력하기
+		System.out.println("합계: " + sum);
+	}
+
+}
